4e088c74948081580fc78649ff90836caff71be2
Torben Hansing <torbenhansing@gmail.com>
2015-07-07 10:13:15 +0200

    GStreamer support with dynamic pipeline

--- src/video/gstreamer.c
+++ src/video/gstreamer.c
@@ -23,17 +23,41 @@
 #include <string.h>
 
 #include <gst/gst.h>
-#include <gst/gstmemory.h>
+#include <gst/video/gstvideodecoder.h>
 
 typedef struct _CustomData {
     GstElement *pipeline;
     GstElement *source;
     GstElement *parser;
     GstElement *decoder;
+    GstElement *deinterlace;
     GstElement *sink;
 } CustomData;
 
 static CustomData data;
+
+static void pad_added(GstElement *src, GstPad *pad, CustomData *data) {
+    GstPad *sink_pad = gst_element_get_static_pad(data->sink, "sink");
+    GstCaps *pad_caps;
+    GstStructure *caps_struct;
+    GstPadLinkReturn ret;
+    const gchar *pad_type;
+
+    pad_caps = gst_pad_query_caps(pad, NULL);
+    caps_struct = gst_caps_get_structure(pad_caps, 0);
+    pad_type = gst_structure_get_name(caps_struct);
+    if (g_str_has_prefix (pad_type, "video/x-raw")) {
+        ret = gst_pad_link (pad, sink_pad);
+        if (GST_PAD_LINK_FAILED (ret)) {
+            g_print ("\n\t[GStreamer] Linking of sink pad failed!\n");
+        }
+    }
+    /* Unreference the new pad's caps, if we got them */
+    if (pad_caps != NULL)
+        gst_caps_unref (pad_caps);
+    /* Unreference the sink pad */
+    gst_object_unref (sink_pad);
+}
 
 bool gstreamer_init() {
 
@@ -42,44 +66,40 @@ bool gstreamer_init() {
 
     /* Create the elements */
     data.source = gst_element_factory_make("appsrc", "video_source");
-    data.parser = gst_element_factory_make("h264parse", "video_parser");
-    data.decoder = gst_element_factory_make("avdec_h264", "video_decoder"); //TODO: How to select decoder dynamically?!
-    data.sink = gst_element_factory_make("glimagesink", "video_sink");
+    data.decoder = gst_element_factory_make("decodebin", "video_decoder");
+    data.sink = gst_element_factory_make("autovideosink", "video_sink");
 
     /* Create the pipeline */
     data.pipeline = gst_pipeline_new("moonlight-embedded");
 
-    if(!data.source || !data.parser || !data.decoder || !data.sink || !data.pipeline) {
-        g_printerr("Error creating Gstreamer elements");
+    if(!data.source || !data.decoder || !data.sink || !data.pipeline) {
+        g_printerr("\n\t[GStreamer] Error creating elements!\n");
         return FALSE;
     }
-    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.parser, data.decoder, data.sink, NULL);
-    /* Link all elements except for the source*/
-    if(!gst_element_link_many(data.parser, data.decoder, data.sink, NULL)) {
-        g_printerr("Error linking GStreamer elements\n");
+    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.decoder, data.sink, NULL);
+
+    /* Link the source*/
+    if(!gst_element_link(data.source, data.decoder)) {
+        g_printerr("\n\t[GStreamer] Error linking source -> decoder\n");
         gst_object_unref(data.pipeline);
         return FALSE;
     }
+
+    /* Register callback on pad-added signal */
+    g_signal_connect(data.decoder, "pad-added", G_CALLBACK(pad_added), &data);
     return true;
 }
 
 void decoder_renderer_setup(int width, int height, int redrawRate, void* context, int drFlags) {
     /* Configure the appsrc */
     GstCaps *video_caps = gst_caps_new_simple("video/x-h264",
-                                              "stream-format", G_TYPE_STRING, "(string)byte-stream",
+                                              "stream-format", G_TYPE_STRING, "byte-stream",
                                               "widht", G_TYPE_INT, width,
                                               "height", G_TYPE_INT, height,
                                               "framerate", GST_TYPE_FRACTION, redrawRate, 1,
                                               NULL);
     g_object_set(data.source, "caps", video_caps, NULL);
     gst_caps_unref(video_caps);
-
-    /* Link the source*/
-    if(!gst_element_link(data.source, data.parser)) {
-        g_printerr("Error linking GStreamer source\n");
-        gst_object_unref(data.pipeline);
-        return;
-    }
 
     /* Start the pipeline */
     gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
